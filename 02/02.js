24ed895d577219c575f9c88a93156631314ea8ca
Heiner Langer <heiner.langer@netlight.com>
2020-12-02 10:18:24 +0100

    day 2 part two done

--- 02/02.js
+++ 02/02.js
@@ -21,7 +21,7 @@ function part1() {
     }
   }
 
-  console.log(validCount);
+  console.log("part one:", validCount);
 
   function isValid(e) {
     const array = e.split(": ");
@@ -44,4 +44,34 @@ function part1() {
   }
 }
 
-function part2() {}
+function part2() {
+  let validCount = 0;
+
+  for (let e of data) {
+    if (isValid(e)) {
+      validCount++;
+    }
+  }
+
+  console.log("part two:", validCount);
+
+  function isValid(e) {
+    const array = e.split(": ");
+    const policy = array[0];
+    const letterToCheck = policy.split(" ")[1];
+    const positionString = policy.split(" ")[0];
+    const pos1 = parseInt(positionString.split("-")[0]);
+    const pos2 = parseInt(positionString.split("-")[1]);
+    const password = array[1];
+    let letterInPos1AndNotInPos2 =
+      password[pos1 - 1] === letterToCheck &&
+      password[pos2 - 1] !== letterToCheck;
+    let letterNotInPos1AndInPos2 =
+      password[pos1 - 1] !== letterToCheck &&
+      password[pos2 - 1] === letterToCheck;
+    if (letterInPos1AndNotInPos2 || letterNotInPos1AndInPos2) {
+      return true;
+    }
+    return false;
+  }
+}
