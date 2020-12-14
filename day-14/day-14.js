cf26e0724fe05cb5b2e379d5f1517503fcdda952
Heiner Langer <heiner.langer@netlight.com>
2020-12-14 12:31:37 +0100

    day 14 part 1

--- day-14/day-14.js
+++ day-14/day-14.js
@@ -1,5 +1,57 @@
+module.exports.Memory = class Memory {
+  data = new Map();
+
+  constructor() {}
+
+  set mask(mask) {
+    this.maskArray = mask.split("");
+  }
+
+  apply(address, value) {
+    const valueArray = this.dec2bin(value).split("");
+    for (let i = 0; i < this.maskArray.length; i++) {
+      if (this.maskArray[i] === "1") {
+        valueArray[i] = "1";
+      }
+      if (this.maskArray[i] === "0") {
+        valueArray[i] = "0";
+      }
+    }
+    this.data.set(address, valueArray.join(""));
+  }
+
+  dec2bin = (dec) => {
+    const shortString = (dec >>> 0).toString(2);
+    const fullString = "000000000000000000000000000000000000";
+    return fullString.substr(0, 36 - shortString.length) + shortString;
+  };
+
+  getSum() {
+    let sum = 0;
+    this.data.forEach((value) => {
+      sum += parseInt(value, 2);
+    });
+    return sum;
+  }
+};
+
 module.exports.part1 = (data) => {
-  return data.length;
+  const memory = new this.Memory();
+  for (let d = 0; d < data.length; d++) {
+    const row = data[d];
+    if (row.startsWith("mask")) {
+      const mask = row.substr(7);
+      memory.mask = mask;
+    } else {
+      const address = parseInt(
+        row.split(" = ")[0].replace("mem[", "").replace("]")
+      );
+      const value = parseInt(row.split(" = ")[1]);
+
+      memory.apply(address, value);
+    }
+  }
+  return memory.getSum();
 };
 
 module.exports.part2 = (data) => {
--- day-14/day-14.test.js
+++ day-14/day-14.test.js
@@ -2,7 +2,40 @@ const day = require("./day-14.js");
 
 describe("day-14", () => {
   describe("part one", () => {
-    test("works with demo", () => {});
+    test("works with demo", () => {
+      const demo = (
+        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\n" +
+        "mem[8] = 11\n" +
+        "mem[7] = 101\n" +
+        "mem[8] = 0"
+      ).split("\n");
+      expect(day.part1(demo)).toEqual(165);
+    });
+
+    test("num to bin converter", () => {
+      const m = new day.Memory();
+      expect(m.dec2bin(11)).toEqual("000000000000000000000000000000001011");
+      expect(m.dec2bin(101)).toEqual("000000000000000000000000000001100101");
+      expect(m.dec2bin(0)).toEqual("000000000000000000000000000000000000");
+    });
+
+    test("apply 11", () => {
+      const m = new day.Memory("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+      m.apply(8, 11);
+      expect(m.data.get(8)).toEqual("000000000000000000000000000001001001");
+    });
+
+    test("apply 101", () => {
+      const m = new day.Memory("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+      m.apply(7, 101);
+      expect(m.data.get(8)).toEqual("000000000000000000000000000001100101");
+    });
+
+    test("apply 0", () => {
+      const m = new day.Memory("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+      m.apply(8, 0);
+      expect(m.data.get(8)).toEqual("000000000000000000000000000001000000");
+    });
   });
 
   describe("part two", () => {
