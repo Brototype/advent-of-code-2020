50ceedd4bac24e5e4f3e7d6f3171e3f87b6a1569
Heiner Langer <heiner.langer@netlight.com>
2020-12-16 15:49:03 +0100

    day 16 part 2

--- day-16/day-16.js
+++ day-16/day-16.js
@@ -1,12 +1,26 @@
 function getRules(data) {
   const rules = [];
   for (let i = 0; i < data.length; i++) {
-    const row = data[i];
-    if (row === "") {
+    const r = data[i];
+    if (r === "") {
       return rules;
     }
-    rules.push(row);
+
+    const split1 = r.split(": ");
+    const name = split1[0];
+    const minAndMax = split1[1].split(" or ");
+    const part1 = minAndMax[0].split("-");
+    const part2 = minAndMax[1].split("-");
+    const mathRule = {
+      type: name,
+      min1: Number(part1[0]),
+      max1: Number(part1[1]),
+      min2: Number(part2[0]),
+      max2: Number(part2[1]),
+    };
+    rules.push(mathRule);
   }
+  return rules;
 }
 
 function getMyTicket(data) {
@@ -34,24 +48,12 @@ function getNearbyTickets(data) {
 }
 
 function getNumbersNotValidForAnyField(ticket, rules) {
-  const mathRules = rules.map((r) => {
-    const split = r.split(" ");
-    const part1 = split[1].split("-");
-    const part2 = split[3].split("-");
-    return {
-      min1: Number(part1[0]),
-      max1: Number(part1[1]),
-      min2: Number(part2[0]),
-      max2: Number(part2[1]),
-    };
-  });
-
   const ticketNumbers = ticket.split(",").map((x) => Number(x));
   const numbersValidForNoRules = [];
 
   ticketNumbers.forEach((ticketNumber) => {
     let validForRules = 0;
-    mathRules.forEach((rule) => {
+    rules.forEach((rule) => {
       if (
         (ticketNumber >= rule.min1 && ticketNumber <= rule.max1) ||
         (ticketNumber >= rule.min2 && ticketNumber <= rule.max2)
@@ -70,7 +72,6 @@ function getNumbersNotValidForAnyField(ticket, rules) {
 module.exports.part1 = (data) => {
   // split the input into rules, myTicket and nearbyTickets
   const rules = getRules(data);
-  const myTicket = getMyTicket(data);
   const nearbyTickets = getNearbyTickets(data);
 
   const numbersNotValidForAnyRule = [];
@@ -81,13 +82,94 @@ module.exports.part1 = (data) => {
     );
   });
 
-  const sum = numbersNotValidForAnyRule.reduce(
+  return numbersNotValidForAnyRule.reduce(
     (previousValue, currentValue) => previousValue + currentValue,
     0
   );
-  return sum;
 };
 
-module.exports.part2 = (data) => {
-  return data.length;
+function findPositionsOfRule(rule, validTickets) {
+  let positionsForRule = [];
+
+  let numbersAtPositions = [];
+  for (let i = 0; i < validTickets.length; i++) {
+    const ticket = validTickets[i];
+    const ticketNumbers = ticket.split(",").map((x) => Number(x));
+    for (let j = 0; j < ticketNumbers.length; j++) {
+      if (!numbersAtPositions[j]) {
+        numbersAtPositions[j] = [];
+      }
+      numbersAtPositions[j] = [...numbersAtPositions[j], ticketNumbers[j]];
+    }
+  }
+
+  for (let pos = 0; pos < numbersAtPositions.length; pos++) {
+    const numbers = numbersAtPositions[pos];
+    let violationsForRuleAtPos = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      const number = numbers[i];
+      if (
+        (number >= rule.min1 && number <= rule.max1) ||
+        (number >= rule.min2 && number <= rule.max2)
+      ) {
+      } else {
+        violationsForRuleAtPos++;
+      }
+    }
+    if (violationsForRuleAtPos === 0) {
+      positionsForRule.push(pos);
+    }
+  }
+
+  return positionsForRule;
+}
+
+module.exports.part2 = (data, ruleStartsWith = "departure") => {
+  // split the input into rules, myTicket and nearbyTickets
+  let rules = getRules(data);
+  const myTicket = getMyTicket(data);
+  const nearbyTickets = getNearbyTickets(data);
+  const validTickets = [];
+
+  nearbyTickets.forEach((ticket) => {
+    if (getNumbersNotValidForAnyField(ticket, rules).length === 0) {
+      validTickets.push(ticket);
+    }
+  });
+
+  rules.forEach((rule) => {
+    const positions = findPositionsOfRule(rule, validTickets);
+    rule.positions = positions;
+  });
+
+  rules.sort((a, b) => a.positions.length - b.positions.length);
+
+  // clean the positions for all of the other rules from the positions that are only 1
+  for (let i = 0; i < rules.length - 1; i++) {
+    const position = rules[i].positions[0];
+    if (rules[i].positions.length > 1) {
+      throw new Error("rethink your algo");
+    }
+    for (let j = i + 1; j < rules.length; j++) {
+      const ru = rules[j];
+      const newPositions = ru.positions.filter((x) => x !== position);
+      ru.positions = newPositions;
+    }
+  }
+
+  const myTicketNumbers = myTicket.split(",").map((x) => Number(x));
+  rules = rules.map((r) => {
+    return {
+      ...r,
+      valueOfMyTicket: myTicketNumbers[r.positions[0]],
+    };
+  });
+
+  const filteredRules = rules.filter((x) => x.type.startsWith(ruleStartsWith));
+
+  const product = filteredRules.reduce((previousValue, currentValue) => {
+    return previousValue * currentValue.valueOfMyTicket;
+  }, 1);
+
+  return product;
 };
--- day-16/day-16.test.js
+++ day-16/day-16.test.js
@@ -5,18 +5,17 @@ describe("day-16", () => {
     test("works with demo", () => {
       const aTicket = "101,102,103,104,301,302,303,401,402,403";
       const demo = (
-        "class: 1-3 or 5-7\n" +
-        "row: 6-11 or 33-44\n" +
-        "seat: 13-40 or 45-50\n" +
+        "class: 0-1 or 4-19\n" +
+        "row: 0-5 or 8-19\n" +
+        "seat: 0-13 or 16-19\n" +
         "\n" +
         "your ticket:\n" +
-        "7,1,14\n" +
+        "11,12,13\n" +
         "\n" +
         "nearby tickets:\n" +
-        "7,3,47\n" +
-        "40,4,50\n" +
-        "55,2,20\n" +
-        "38,6,12"
+        "3,9,18\n" +
+        "15,1,5\n" +
+        "5,14,9"
       ).split("\n");
 
       expect(day.part1(demo)).toBe(71);
@@ -24,6 +23,22 @@ describe("day-16", () => {
   });
 
   describe("part two", () => {
-    test("works with demo", () => {});
+    test("works with demo", () => {
+      const demo = (
+        "class: 0-1 or 4-19\n" +
+        "row: 0-5 or 8-19\n" +
+        "seat: 0-13 or 16-19\n" +
+        "\n" +
+        "your ticket:\n" +
+        "11,12,13\n" +
+        "\n" +
+        "nearby tickets:\n" +
+        "3,9,18\n" +
+        "15,1,5\n" +
+        "5,14,9"
+      ).split("\n");
+
+      expect(day.part2(demo, "class")).toBe(12);
+    });
   });
 });
