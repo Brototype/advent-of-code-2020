4179c4677d016c10c52ea64536770640238ab3fc
Heiner Langer <heiner.langer@netlight.com>
2020-12-19 17:13:52 +0100

    day 19 part 2

--- day-19/day-19.js
+++ day-19/day-19.js
@@ -3,10 +3,18 @@ module.exports.part1 = (data) => {
   const rules = data.filter((d) => d.includes(":"));
   const testStrings = data.filter((d) => d.length > 0 && !d.includes(":"));
   const regexTester = new RegexTester(rules);
-  regexTester.createRegex();
+  regexTester.getRegexpForRule(regexTester.cleanRules[0]);
   return regexTester.numberOfMatching(testStrings);
 };
 
 module.exports.part2 = (data) => {
-  return data.length;
+  const rules = data.filter((d) => d.includes(":"));
+  const testStrings = data.filter((d) => d.length > 0 && !d.includes(":"));
+  const regexTester = new RegexTester(rules);
+
+  regexTester.getRegexpForRule(regexTester.cleanRules[0]);
+  // replace with loop rules
+  regexTester.replaceSpecialRules();
+
+  return regexTester.numberOfMatchingWithGrouping(testStrings);
 };
--- day-19/regex-tester.js
+++ day-19/regex-tester.js
@@ -1,11 +1,13 @@
 module.exports.RegexTester = class RegexTester {
+  ruleToRegExpString = new Map();
+
   constructor(rules = []) {
-    this.cleanRules = [];
+    this.cleanRules = {};
     rules.forEach((rule) => {
       // = rules.map((r) => r.split(": ")[1].split(" "));
       let pureRuleString = rule.split(": ")[1];
-      let address = Number(rule.split(": ")[0]);
-      // replace the weird quotes
+      let number = rule.split(": ")[0];
+      //  replace the weird quotes
       if (pureRuleString === '"a"') {
         pureRuleString = "a";
       }
@@ -13,37 +15,79 @@ module.exports.RegexTester = class RegexTester {
       if (pureRuleString === '"b"') {
         pureRuleString = "b";
       }
-      //surround options with brackets already
-      if (pureRuleString.includes("|")) {
-        pureRuleString = "( " + pureRuleString + " )";
-      }
-      this.cleanRules[address] = pureRuleString;
+      this.cleanRules[number] = pureRuleString;
     });
   }
 
-  createRegex() {
-    let firstRuleContainsDigits = /\d/.test(this.cleanRules[0]);
-    while (firstRuleContainsDigits) {
-      this.replaceDigitsInMainRule();
-      firstRuleContainsDigits = /\d/.test(this.cleanRules[0]);
-    }
-    const currentRuleWithoutSpaces =
-      "^" + this.cleanRules[0].split(" ").join("") + "$";
-
-    this.regex = new RegExp(currentRuleWithoutSpaces, "i");
+  replaceSpecialRules() {
+    this.cleanRules["8"] = "42 | 42 8";
+    this.cleanRules["11"] = "42 31 | 42 11 31";
   }
 
-  replaceDigitsInMainRule() {
-    const split = this.cleanRules[0].split(" ");
-    for (let i = 0; i < split.length; i++) {
-      if (Number.isInteger(Number(split[i]))) {
-        split[i] = this.cleanRules[split[i]];
-      }
+  getRegexpForRule(rule) {
+    if (this.ruleToRegExpString.has(rule)) {
+      return this.ruleToRegExpString.get(rule);
     }
-    this.cleanRules[0] = split.join(" ");
+    const re = this.createRegexpStringRecursive(rule);
+    const regExp = new RegExp("^" + re + "$", "i");
+    this.regex = regExp;
+    return regExp;
+  }
+
+  createRegexpStringRecursive(rule) {
+    let regexpString = "";
+
+    if (rule === "a" || rule === "b") {
+      // this is just a or b
+      regexpString = rule;
+    } else if (rule.includes("|")) {
+      // in case we see the options pipe
+      const options = rule.split(" | ");
+      regexpString = `(${this.createRegexpStringRecursive(
+        options[0]
+      )}|${this.createRegexpStringRecursive(options[1])})`;
+    } else {
+      // simple reference case
+      const ruleNumbers = rule.split(" ");
+      regexpString = ruleNumbers
+        .map((ruleNumber) =>
+          this.createRegexpStringRecursive(this.cleanRules[ruleNumber])
+        )
+        .join("");
+    }
+    this.ruleToRegExpString.set(rule, regexpString);
+    return regexpString;
   }
 
   numberOfMatching(input) {
     return input.filter((testString) => this.regex.test(testString)).length;
+  }
+
+  numberOfMatchingWithGrouping(inputs) {
+    const pattern =
+      "^(?<group42>(" +
+      this.ruleToRegExpString.get(this.cleanRules[42]) +
+      ")+)(?<group31>(" +
+      this.ruleToRegExpString.get(this.cleanRules[31]) +
+      ")+)$";
+    const rule = new RegExp(pattern);
+
+    let sum = 0;
+    for (const input of inputs) {
+      const matches = rule.exec(input);
+      if (matches) {
+        const { groups } = matches;
+        const matches42 = groups.group42.match(
+          new RegExp(this.ruleToRegExpString.get(this.cleanRules[42]), "g")
+        ).length;
+        const matches31 = groups.group31.match(
+          new RegExp(this.ruleToRegExpString.get(this.cleanRules[31]), "g")
+        ).length;
+        if (matches42 > matches31) {
+          sum++;
+        }
+      }
+    }
+    return sum;
   }
 };
