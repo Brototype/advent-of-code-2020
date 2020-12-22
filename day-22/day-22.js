1e77c2d4d65d41a74350b79d2b72cbc6be87536b
Heiner Langer <heiner.langer@netlight.com>
2020-12-22 09:58:23 +0100

    day 22 part 2

--- day-22/day-22.js
+++ day-22/day-22.js
@@ -1,31 +1,34 @@
 module.exports.part1 = (data) => {
   const rawInput = data.join("\n");
   const split = rawInput.split("\n\n");
-  let player1Deck = getDeck(split[0]);
-  let player2Deck = getDeck(split[1]);
+  const decks = [getDeck(split[0]), getDeck(split[1])];
 
   //play
-  while (!(player1Deck.length === 0 || player2Deck.length === 0)) {
-    const cardOfPlayer1 = player1Deck.shift();
-    const cardOfPlayer2 = player2Deck.shift();
+  while (!(decks[0].length === 0 || decks[1].length === 0)) {
+    // deal card
+    const cardOfPlayer1 = decks[0].shift();
+    const cardOfPlayer2 = decks[1].shift();
+
+    // compare dealt cards and handle result
     if (cardOfPlayer1 > cardOfPlayer2) {
-      player1Deck.push(cardOfPlayer1);
-      player1Deck.push(cardOfPlayer2);
+      decks[0].push(cardOfPlayer1);
+      decks[0].push(cardOfPlayer2);
     } else {
-      player2Deck.push(cardOfPlayer2);
-      player2Deck.push(cardOfPlayer1);
+      decks[1].push(cardOfPlayer2);
+      decks[1].push(cardOfPlayer1);
     }
   }
 
-  //calculate score
-  const player1Score = calculateScore(player1Deck);
-  const player2Score = calculateScore(player2Deck);
-
-  if (player1Score > player2Score) {
-    return player1Score;
+  // get winner
+  let winner;
+  if (decks[0].length === 0) {
+    winner = 1;
   } else {
-    return player2Score;
+    winner = 0;
   }
+
+  //calculate score
+  return calculateScore(decks[winner]);
 };
 
 function calculateScore(deck) {
@@ -43,5 +46,50 @@ function getDeck(blob) {
 }
 
 module.exports.part2 = (data) => {
-  return data.length;
+  const rawInput = data.join("\n");
+  const split = rawInput.split("\n\n");
+  const decks = [getDeck(split[0]), getDeck(split[1])];
+  let winner = playRecursive(decks);
+  return calculateScore(decks[winner]);
 };
+
+function playRecursive(decks) {
+  let previous = new Set();
+  while (decks[0].length > 0 && decks[1].length > 0) {
+    if (previous.has(decks[0] + " " + decks[1])) {
+      return 0;
+    }
+
+    previous.add(decks[0] + " " + decks[1]);
+    let player1Card = decks[0].shift();
+    let player2Card = decks[1].shift();
+
+    let winner;
+    if (player1Card > player2Card) {
+      winner = 0;
+    } else {
+      winner = 1;
+    }
+
+    if (decks[0].length >= player1Card && decks[1].length >= player2Card) {
+      winner = playRecursive([
+        decks[0].slice(0, player1Card),
+        decks[1].slice(0, player2Card),
+      ]);
+    }
+    if (winner === 0) {
+      decks[0].push(player1Card, player2Card);
+    } else {
+      decks[1].push(player2Card, player1Card);
+    }
+  }
+
+  let winner;
+  if (decks[0].length) {
+    winner = 0;
+  } else {
+    winner = 1;
+  }
+
+  return winner;
+}
--- day-22/day-22.test.js
+++ day-22/day-22.test.js
@@ -1,28 +1,30 @@
 const day = require("./day-22.js");
 
 describe("day-22", () => {
+  const input = (
+    "Player 1:\n" +
+    "9\n" +
+    "2\n" +
+    "6\n" +
+    "3\n" +
+    "1\n" +
+    "\n" +
+    "Player 2:\n" +
+    "5\n" +
+    "8\n" +
+    "4\n" +
+    "7\n" +
+    "10"
+  ).split("\n");
   describe("part one", () => {
     test("works with demo", () => {
-      const input = (
-        "Player 1:\n" +
-        "9\n" +
-        "2\n" +
-        "6\n" +
-        "3\n" +
-        "1\n" +
-        "\n" +
-        "Player 2:\n" +
-        "5\n" +
-        "8\n" +
-        "4\n" +
-        "7\n" +
-        "10"
-      ).split("\n");
       expect(day.part1(input)).toBe(306);
     });
   });
 
   describe("part two", () => {
-    test("works with demo", () => {});
+    test("works with demo", () => {
+      expect(day.part2(input)).toBe(291);
+    });
   });
 });
